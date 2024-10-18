198f894aa2595e79d84465c5c3e43a4303f1710d
stefbell <144052180+stefbell@users.noreply.github.com>
2024-10-18 09:45:36 -0400

    camera working

---
+++ haunted-house/Assets/mouseFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float rotationX = 0f;
+    float rotationY = 0f;
+    public float sensitivity = 15f;
+    public Camera mainCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotationY += Input.GetAxis("Mouse X") * sensitivity;
+        rotationX += Input.GetAxis("Mouse Y") * -1 * sensitivity;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);  
+        transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+    }
+}
